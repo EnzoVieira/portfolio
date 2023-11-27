b78ab7a013a237d5a570714923f6ce6865703f85
Enzo Vieira <enzovieira16@hotmail.com>
2023-11-27 20:10:29 +0000

    feat: projects page is now MDX

---
+++ app/(projects)/[...slug]/page.tsx
@@ -0,0 +1,99 @@
+import { Metadata } from "next";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { allProjects } from "contentlayer/generated";
+
+import { StaggerParagraph } from "@/app/components/StaggerParagraph";
+import { StaggerText } from "@/app/components/StaggerText";
+import { Mdx } from "@/components/mdx-components";
+
+interface ProjectProps {
+  params: {
+    slug: string[];
+  };
+}
+
+async function getProjectFromParams(params: ProjectProps["params"]) {
+  const slug = params?.slug?.join("/");
+  const project = allProjects.find((project) => project.slugAsParams === slug);
+
+  if (!project) {
+    null;
+  }
+
+  return project;
+}
+
+// export async function generateMetadata({
+//   params,
+// }: ProjectProps): Promise<Metadata> {
+//   const post = await getProjectFromParams(params);
+
+//   if (!post) {
+//     return {};
+//   }
+
+//   return {
+//     title: post.title,
+//     description: post.description,
+//   };
+// }
+
+export async function generateStaticParams(): Promise<
+  ProjectProps["params"][]
+> {
+  return allProjects.map((project) => ({
+    slug: project.slugAsParams.split("/"),
+  }));
+}
+
+export default async function InvlancePage({ params }: ProjectProps) {
+  const project = await getProjectFromParams(params);
+
+  if (!project) {
+    return notFound();
+  }
+
+  return (
+    <main>
+      <StaggerText className="normal-case">Invlance</StaggerText>
+
+      <dl className="pt-10">
+        <dt className="text-xs opacity-50 uppercase">
+          <StaggerParagraph>Role / Services</StaggerParagraph>
+        </dt>
+        <div className="scale-x-0 h-[1px] w-full bg-black opacity-[.15] animate-divider-scale origin-left my-4" />
+        <dd className="pb-8">
+          <StaggerParagraph>{project.role}</StaggerParagraph>
+        </dd>
+
+        <dt className="text-xs opacity-50 uppercase ">
+          <StaggerParagraph>Briefing</StaggerParagraph>
+        </dt>
+        <div className="scale-x-0 h-[1px] w-full bg-black opacity-[.15] animate-divider-scale origin-left my-4" />
+        <dd className="pb-8">
+          <StaggerParagraph>{project.briefing}</StaggerParagraph>
+        </dd>
+
+        <dt className="text-xs opacity-50 uppercase">
+          <StaggerParagraph>Year</StaggerParagraph>
+        </dt>
+        <div className="scale-x-0 h-[1px] w-full bg-black opacity-[.15] animate-divider-scale origin-left my-4" />
+        <dd className="pb-8">
+          <StaggerParagraph>{project.year}</StaggerParagraph>
+        </dd>
+      </dl>
+
+      <StaggerParagraph className="pb-2">
+        {project.description}
+      </StaggerParagraph>
+
+      <StaggerParagraph className="text-8xl text-center pb-20">
+        &darr;
+      </StaggerParagraph>
+
+      <hr className="my-4" />
+      <Mdx code={project.body.code} />
+    </main>
+  );
+}
---
+++ contentlayer.config.js
@@ -0,0 +1,58 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+
+/** @type {import('contentlayer/source-files').ComputedFields} */
+const computedFields = {
+  slug: {
+    type: "string",
+    resolve: (doc) => `/${doc._raw.flattenedPath}`,
+  },
+  slugAsParams: {
+    type: "string",
+    resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/"),
+  },
+};
+
+export const Page = defineDocumentType(() => ({
+  name: "Page",
+  filePathPattern: `pages/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    description: {
+      type: "string",
+    },
+  },
+  computedFields,
+}));
+
+export const Project = defineDocumentType(() => ({
+  name: "Project",
+  filePathPattern: `projects/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    role: {
+      type: "string",
+      required: true,
+    },
+    briefing: {
+      type: "string",
+    },
+    year: {
+      type: "string",
+      required: true,
+    },
+    description: {
+      type: "string",
+      required: true,
+    },
+  },
+  computedFields,
+}));
+
+export default makeSource({
+  contentDirPath: "./content",
+  documentTypes: [Project, Page],
+});
