d73f257ac47b9d2a538724e6fb9b82bcba505c1b
Enzo Vieira <enzovieira16@hotmail.com>
2023-11-28 22:41:32 +0000

    feat(layout): add Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,12 +18,31 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  function getLocalDateFormatted() {
+    // Obtém a data e hora locais
+    const now = new Date();
+
+    // Cria um objeto Intl.DateTimeFormat com o fuso horário de Lisboa (GMT+1)
+    const options = { timeZone: "Europe/Lisbon" };
+    const datetimeToFormat = new Intl.DateTimeFormat("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      timeZoneName: "short",
+      ...options,
+    });
+
+    // Formata a data e hora
+    const formattedDate = datetimeToFormat.format(now);
+
+    return formattedDate;
+  }
+
   return (
     <html lang="pt-BR">
       <body
         className={classNames(
           montserrat.className,
-          "px-4 pb-16 w-screen max-w-6xl mx-auto",
+          "px-4 w-screen max-w-6xl mx-auto",
         )}
       >
         <header className="-translate-y-full animate-translate-header py-6 justify-between flex font-medium">
@@ -33,6 +52,41 @@ export default function RootLayout({
         </header>
 
         {children}
+
+        <footer className="pb-16">
+          <h5 className="uppercase text-xs opacity-50 pb-2">Socials</h5>
+
+          <ul className="flex flex-wrap gap-4 items-center">
+            <li>
+              <a
+                href="https://www.linkedin.com/in/enzo-vieira/?locale=en_US"
+                target="_blank"
+              >
+                LinkedIn
+              </a>
+            </li>
+
+            <li>
+              <a href="https://github.com/EnzoVieira" target="_blank">
+                GitHub
+              </a>
+            </li>
+          </ul>
+
+          <div className="h-[1px] w-full bg-black opacity-[.15] my-8" />
+
+          <div className="flex justify-between">
+            <div>
+              <h5 className="text-xs uppercase opacity-50 pb-2">Version</h5>
+              <p>2023 Edition</p>
+            </div>
+
+            <div>
+              <h5 className="text-xs uppercase opacity-50 pb-2">Local Time</h5>
+              <p>{getLocalDateFormatted()}+1</p>
+            </div>
+          </div>
+        </footer>
 
         <Analytics />
       </body>
